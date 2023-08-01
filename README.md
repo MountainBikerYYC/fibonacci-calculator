d7f97aa91001e33c432a75bd34a2277db2ee70b5
MountainBikerYYC <ywang9696@gmail.com>
2023-07-31 22:25:24 -0600

    Completed calculation, save results to database
    Completed calculation, save results to database
    
    Completed calculation, save results to database

---
+++ README.md
@@ -0,0 +1,47 @@
+# Fibonacci Calculator
+
+Updated: August 1st 2023
+
+# Live Application
+
+The project is hosted live on Heroku at
+[fibonacci-calculator.herokuapp.com](https://fibonacci-calculator.herokuapp.com)
+
+# Introduction
+
+Simple Fibonacci Calculator done for the Chan-Zuckerberg Biohub challenge
+
+# Instruction
+
+# If downloading on local machine
+
+1. Go to the folder in fibonacci-calculator
+2. Open a console and create a virtual environment `python3 -m venv venv`
+3. Activate the environment using `source venv/bin/activate`, you should now see (venv) or (main)
+4. Install all dependencies `pip install -r requirements.txt`
+
+Your directory should look like this
+.fibonacci-calculator-app/
+├── static/
+├── templates/
+├── venv/
+|
+├── .gitignore
+├── app.py
+├── fibonacci.db
+├── README
+└── requirements.txt
+
+# Database setup
+
+# We will be using SQlite3 which is a very light weight relational database to store our results
+
+5. Open a new bash console, run the command `sqlite3 fibonacci.db`, you will enter the database shell
+6. To clear the tables, run the command `DROP TABLE fibonacci;`.
+
+# You are now ready to run the application
+
+7. Inside the console with virtual environment, run `python app.py` and open localhost:5000 on your browser.
+8. Enter a number, click generate and it will redirect you to the results page.
+
+# If you enter the same number again, it will calculate much faster since the results are already stored in the database!
--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, redirect, g
 import sqlite3
-import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['DATABASE'] = 'fibonacci.db'
 
+# Create empty database
 def create_tables():
     with get_db() as db:
         cursor = db.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS fibonacci (n INTEGER PRIMARY KEY, result TEXT)")
 
+# Calculation
 def fibonacci(n):
     if n <= 0:
         return []
@@ -19,29 +19,31 @@ def fibonacci(n):
     elif n == 2:
         return [0, 1]
 
+    # Check in database if already computed
     with get_db() as db:
         cursor = db.cursor()
         cursor.execute("SELECT result FROM fibonacci WHERE n=?", (n,))
         result = cursor.fetchone()
+        # if yes, return that entire result array
         if result:
-            # If result exists in the database, retrieve it and return
             fib_str = result[0]
             return result[0]
 
-        # If result doesn't exist in the database, compute the Fibonacci numbers and store them in the database
         fib = [0, 1]
+        # compute the numbers
         while len(fib) < n:
             fib.append(fib[-1] + fib[-2])
 
-        # Convert the list of Fibonacci numbers to a comma-separated string
-        fib_str = ', '.join(map(str, fib))
+        # Convert and seperate using commas
+        fib_result = ', '.join(map(str, fib))
 
-        # Save the computed result to the database
-        cursor.execute("INSERT OR REPLACE INTO fibonacci (n, result) VALUES (?, ?)", (n, fib_str))
+        # Save the resulting array to the database
+        cursor.execute("INSERT OR REPLACE INTO fibonacci (n, result) VALUES (?, ?)", (n, fib_result))
         db.commit()
 
         return fib
 
+# Initialize database connection
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -49,33 +51,28 @@ def get_db():
         create_tables()
     return db
 
+# Close database
 def close_db(e=None):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-# Rest of your code remains the same...
-
+# index page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         n = int(request.form.get('n'))
-        fib_numbers = fibonacci(n)
 
         return redirect(f'/fibonacci/{n}')
 
     return render_template('index.html')
 
+# results page
 @app.route('/fibonacci/<int:n>')
-def show_fibonacci_numbers(n):
-    # Retrieve the Fibonacci numbers from the database
+def fibonacci_results(n):
 
-    # start_time = time.time()
     fib_result = fibonacci(n)
-    # end_time = time.time()
-    # print(f"Time taken to compute Fibonacci({n}): {end_time - start_time:.6f} seconds")
-
-    return render_template('fibonacci_numbers.html', n=n, fib_result=fib_result)
+    return render_template('fibonacci_results.html', n=n, fib_result=fib_result)
 
 if __name__ == '__main__':
     app.teardown_appcontext(close_db)
